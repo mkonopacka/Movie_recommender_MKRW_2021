7527c80f519da131f93aa8475879313170e0cad7
wojt <abc.@interia.pl>
2021-03-21 22:07:34 +0100

    added SVD2

--- recom_system_307915_300801.py
+++ recom_system_307915_300801.py
@@ -30,29 +30,45 @@ def RMSE(Zp):
         RMSE = RMSE + (Zp[i][j] - k)**2
     return sqrt(RMSE/test.shape[0])
 
-def test_NMF(Z_, r = 10, print = False):
+def test_NMF(Z_, r = 10, comm = False):
     ''' Nonnegative Matrix Factorization 
         Z_(nd.array) original matrix
         r (float) number of features'''
-    if print: print('Building a model...')
+    if comm: print('Building a model...')
     model = NMF(n_components = r, init = 'random', random_state = 77)
     W = model.fit_transform(Z_)
     H = model.components_
     Z_approx = np.dot(W,H)
-    if print: print('Model finished.')
+    if comm: print('Model finished.')
     result = RMSE(Z_approx)
-    if print: print(f"RMSE for matrix Z' (Z approximated with NMF): {result}")
+    if comm: print(f"RMSE for matrix Z' (Z approximated with NMF): {result}")
     return result
 
-def test_SVD1(Z_, r = 3, print = False):
-    ''' Singular Value Decomposition; r: rank '''
-    if print: print('Building a model...')
-    U, S, VT = np.linalg.svd(Z_, full_matrices = False)
+def SVD(Z, r):
+    U, S, VT = np.linalg.svd(Z, full_matrices = False)
     S = np.diag(S)
     Z_approx = U[:,:r] @ S[0:r,:r] @ VT[:r,:]
-    if print: print('Model finished.')
+    return Z_approx
+
+def test_SVD1(Z_, r = 3, comm = False):
+    ''' Singular Value Decomposition; r: rank '''
+    if comm: print('Building a model...')
+    Z_approx = SVD(Z_, r)
+    if comm: print('Model finished.')
     result = RMSE(Z_approx)
-    if print: print(f"RMSE for matrix Z' (Z approximated with SVD1 with rank = {r}): {result}")
+    if comm: print(f"RMSE for matrix Z' (Z approximated with SVD1 with rank = {r}): {result}")
+    return result
+    
+def test_SVD2(Z_, i=3, r=5, comm = False):
+    ''' SVD2; i: number of iterations; r: rank'''
+    if comm: print('Building a model...')
+    Z_approx = np.copy(Z_)
+    for j in tqdm(range(i)):
+        Z_approx = SVD(Z_approx, r)
+        if j != i-1: train_matrix(Z_approx)
+    if comm: print('Model finished.')
+    result = RMSE(Z_approx)
+    if comm: print(f"RMSE for matrix Z' (Z approximated with SVD2 with rank = {r}, nr of iterations = {i}): {result}")
     return result
 
 # %% 
@@ -67,17 +83,26 @@ n = len(all_users) # 610
 
 # %% Create matrix Z_avg
 avg_rating = np.mean(train.rating) 
-Z = np.full((n,d), avg_rating)
-for row in tqdm(range(train.shape[0])):
-    ''' Id userów są od 1 do 610 i jest ich 610 więc odejmujemy 1, a Id filmów
-        jest dużo mniej niż ich max. więc zamiast id bierzemy jego numer w posortowanej liście '''
-    i = train.userId[row] - 1
-    j = all_movies.index(train.movieId[row])
-    k = train.rating[row]
-    Z[i][j] = k
+Z_avg = np.full((n,d), avg_rating)
 
-Z_avg = Z
-print('RMSE for original matrix Z: ', RMSE(Z))
+def train_matrix(Z):
+    '''fills matrix Z with training data'''
+    for row in tqdm(range(train.shape[0])):
+        ''' Id userów są od 1 do 610 i jest ich 610 więc odejmujemy 1, a Id filmów
+            jest dużo mniej niż ich max. więc zamiast id bierzemy jego numer w posortowanej liście '''
+        i = train.userId[row] - 1
+        j = all_movies.index(train.movieId[row])
+        k = train.rating[row]
+        Z[i][j] = k
+    return 
+
+#train_matrix(Z_avg)
+#print('RMSE for original matrix Z: ', RMSE(Z_avg))
+
+user_avgs = np.array(train.groupby('userId')['rating'].mean())
+Z_avg_user = np.repeat(user_avgs.reshape((1, -1)), n, axis = 0)
+train_matrix(Z_avg_user)
+print('RMSE for original matrix Z_avg_user: ', RMSE(Z_avg_user))
 
 # %% TODO Create matrix Z_avg_user: wypełnianie średnią oceną dla danego użytkownika
 
@@ -89,10 +114,12 @@ print('RMSE for original matrix Z: ', RMSE(Z))
 # %% TESTY
 
 # %% Program
-'''
+
 if args.alg == 'NMF': 
-    test_NMF(Z_avg, print = True)
+    test_NMF(Z_avg, r=10, comm = True)
 
 if args.alg == 'SVD1': 
-    test_SVD1(Z_avg, 3, print = True)
-'''
+    test_SVD1(Z_avg_user, r=10, comm = True)
+
+if args.alg == 'SVD2': 
+    test_SVD2(Z_avg, i=3, r=10, comm = True)
