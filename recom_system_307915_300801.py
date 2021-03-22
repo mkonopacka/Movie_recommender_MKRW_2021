a2fe286ee982a4daeb66921799f2fc5dd0553332
mkonopacka <67000987+mkonopacka@users.noreply.github.com>
2021-03-22 10:50:42 +0100

    add Z_avg_movie, Z_avg_user

--- recom_system_307915_300801.py
+++ recom_system_307915_300801.py
@@ -3,7 +3,7 @@ import argparse
 import numpy as np
 import pandas as pd
 from tqdm import tqdm 
-from sklearn.decomposition import NMF
+from sklearn.decomposition import NMF, TruncatedSVD
 from math import sqrt
 
 def parse_arguments():
@@ -19,7 +19,6 @@ args = parse_arguments()
 train = pd.read_csv(args.train, index_col= False)
 test = pd.read_csv(args.test, index_col= False)
 
-# %% Algorytmy
 def RMSE(Zp):
     ''' Root-mean-square error between matrix Zp and test matrix '''
     RMSE = 0
@@ -30,6 +29,7 @@ def RMSE(Zp):
         RMSE = RMSE + (Zp[i][j] - k)**2
     return sqrt(RMSE/test.shape[0])
 
+# %% Algorytmy
 def test_NMF(Z_, r = 10, print = False):
     ''' Nonnegative Matrix Factorization 
         Z_(nd.array) original matrix
@@ -65,29 +65,61 @@ test_users = test.userId.unique()
 all_users = np.concatenate((train_users, test_users[~ np.isin(test_users, train_users)]))
 n = len(all_users) # 610
 
-# %% Create matrix Z_avg
+# %% Create matrix Z_avg: wypełnianie średnim rankingiem spośród wszystkich ocenionych filmów w zbiorze treningowym
 avg_rating = np.mean(train.rating) 
-Z = np.full((n,d), avg_rating)
+Z_avg = np.full((n,d), avg_rating)
 for row in tqdm(range(train.shape[0])):
     ''' Id userów są od 1 do 610 i jest ich 610 więc odejmujemy 1, a Id filmów
         jest dużo mniej niż ich max. więc zamiast id bierzemy jego numer w posortowanej liście '''
     i = train.userId[row] - 1
     j = all_movies.index(train.movieId[row])
     k = train.rating[row]
-    Z[i][j] = k
+    Z_avg[i][j] = k
 
-Z_avg = Z
-print('RMSE for original matrix Z: ', RMSE(Z))
+# print('RMSE for original matrix Z_avg: ', RMSE(Z_avg))
+# %% Create matrix Z_avg_user: wypełnianie średnią oceną dla danego użytkownika w zbiorze treningowym
+user_avgs = np.array(train.groupby('userId')['rating'].mean())
+Z_avg_user = np.repeat(user_avgs, d).reshape(n,d)
+for row in tqdm(range(train.shape[0])):
+    ''' Id userów są od 1 do 610 i jest ich 610 więc odejmujemy 1, a Id filmów
+        jest dużo mniej niż ich max. więc zamiast id bierzemy jego numer w posortowanej liście '''
+    i = train.userId[row] - 1
+    j = all_movies.index(train.movieId[row])
+    k = train.rating[row]
+    Z_avg_user[i][j] = k
 
-# %% TODO Create matrix Z_avg_user: wypełnianie średnią oceną dla danego użytkownika
+# print('RMSE for original matrix Z_avg_user: ', RMSE(Z_avg_user))
 
-# %% TODO Create matrix Z_avg_movie: wypełnianie średnią oceną dla danego filmu
+# # %% TODO Create matrix Z_avg_movie: wypełnianie średnią oceną dla danego filmu w zbiorze treningowym
+# #         i średnią wszystkich filmów dla filmów których w nim nie ma
+movie_avgs = train.groupby('movieId')['rating'].mean()
+movie_row = np.repeat(avg_rating, d)
+for id, rating in movie_avgs.iteritems():
+    movie_row[all_movies.index(id)] = rating
+Z_avg_movie = np.array([movie_row]*n)
 
+# # missing_ids = [id for id in test_movies if not id in train_movies]
+# # missing_avgs = pd.Series(np.repeat(avg_rating, len(missing_ids)), index= missing_ids)
+# movie_avgs = np.array(movie_avgs.append(missing_avgs)) # array ze średnimi wszystkich filmów 9724
+# # Tworzymy macierz przez powtórzenie wiersza średnich ocen filmów 610 razy
+# # a następnie wypełnienie znanymi wartościami ze zbioru treningowego
+# Z_avg_movie = np.array([movie_avgs]*n)
+for row in tqdm(range(train.shape[0])):
+    ''' Id userów są od 1 do 610 i jest ich 610 więc odejmujemy 1, a Id filmów
+        jest dużo mniej niż ich max. więc zamiast id bierzemy jego numer w posortowanej liście '''
+    i = train.userId[row] - 1
+    j = all_movies.index(train.movieId[row])
+    k = train.rating[row]
+    Z_avg_user[i][j] = k
+
+# print('RMSE for original matrix Z_avg_movie: ', RMSE(Z_avg_movie))
 # %% TODO Create matrix Z_perc: wypełnianie oceną odpowiadającą percentylem oceny filmu ocenie użytkownika
 
-# %% TODO Create matrix Z_cluster: wypełnianie średnią oceną w jakiejś grupie filmów? Do tego potrzebny clustering
+# %% TODO średnia po filmie i po użytkowniku 2 sposoby
 # %% TESTY
-
+for Z in [Z_avg, Z_avg_movie, Z_avg_user]:
+    print(test_SVD1(Z))
+    print(test_NMF(Z))
 # %% Program
 '''
 if args.alg == 'NMF': 
